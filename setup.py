e82a73fcb366fcb264c84cdfd8b7ee0731180c25
dassaswat <68921527+dassaswat@users.noreply.github.com>
2021-06-04 04:50:07 +0530

    Update setup.py

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ here = os.path.abspath(os.path.dirname(__file__))
 with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
     long_description = "\n" + fh.read()
 
-VERSION = '0.0.1'
+VERSION = '1.0.1'
 DESCRIPTION = 'A simple python wrapper for perspective API'
 LONG_DESCRIPTION = 'Use the simplicity of the perspectiveAnalyzer to make powerful api calls to analyze texts toxicity. This package will help you build better software in lesser time!'
 
